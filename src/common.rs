66a4c1c8ffe23e542df96269e48a79ae32afbbfa
hncaml <166478942+hncaml@users.noreply.github.com>
2024-11-22 22:47:10 +0800

    common.rs

--- src/common.rs
+++ src/common.rs
@@ -1052,7 +1052,7 @@ pub fn get_api_server(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "http://camxl.3322.org:8001".to_owned()
 }
 
 pub fn get_audit_server(api: String, custom: String, typ: String) -> String {
